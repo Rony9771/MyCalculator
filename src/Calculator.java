dd2b02b735c3a5f1f0929d42a67973909c3b188a
Rony9771 <rony.ahmed9771@gmail.com>
2020-02-10 02:02:55 +0600

    first commit

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+
+public class Calculator {
+	
+	public int sub(int n1,int n2) {
+		return n1-n2;
+	}
+	public int add(int n3,int n4) {
+		return n3+n4;
+	}
+
+}
---
+++ src/CalculatorTest.java
@@ -0,0 +1,15 @@
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		 
+		Calculator cal =new Calculator();
+		int result=cal.add(123, 234);
+		int result2=cal.sub(45, 65);
+		
+		System.out.println(result);
+		System.out.println(result2);
+
+	}
+
+}
